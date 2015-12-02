0e1109a8be4ebc64771683768138a90f80d313f9
adityagampa <adityagampa149@gmail.com>
2015-12-02 19:37:59 +0530

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -20,6 +20,50 @@ NOTES: Don't create new string.
 #include <stdio.h>
 
 
-void number_to_str(float number, char *str,int afterdecimal){
-	
+void number_to_str(float n, char *s, int d)
+{
+	int j = n, i = 0, len = 0, flag = 0;
+	if (n<0)
+	{
+		s[i++] = '-';
+		n = n*-1;
+		j = n;
+		flag = 1;
+	}
+	float dec;
+	dec = n - j;
+	char temp;
+	while (j>0)
+	{
+		s[i] = j % 10 + 48;
+		j = j / 10;
+		i++;
+	}
+	s[i] = '\0';
+	len = i;
+	for (j = flag, i = i - 1; j<=len / 2; i--, j++)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+	if (d != 0)
+	{
+		s[len] = '.';
+		len++;
+		for (i = 0; i<d; i++)
+		{
+			dec = dec * 10;
+		}
+		j = dec;
+		s[len + d] = '\0';
+		while (j>0)
+		{
+			d--;
+			s[len + d] = j % 10 + 48;
+			j = j / 10;
+			i++;
+
+		}
+	}
 }
